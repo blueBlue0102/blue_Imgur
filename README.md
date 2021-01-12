9574773f1043a5ed6aaa1ecd949312c048d0321e
Blue <ray1997860102@gmail.com>
2021-01-13 00:24:39 +0800

    可以上傳任何檔案到"img"資料夾內

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # 私人 Imgur 服務
 Node.js 環境
-```
-npm i
-```
+express
+formidable
--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const express = require('express')
+const express = require('express');
+const formidable = require('formidable');
+const fs = require('fs');
+
 const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile('/upload.html', {root: __dirname });
+})
+
+app.post('/fileupload', (req, res) => {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldPath = files.filetoupload.path;
+      var newPath = __dirname + '/img' + '/' + files.filetoupload.name;
+      var rawData = fs.readFileSync(oldPath)
+      fs.writeFile(newPath, rawData, function (err) {
+        if (err){
+          res.send('ERROR!');
+        }
+        else {
+          res.write('File uploaded and moved!');
+          res.end();
+        }
+      });
+    });
+})
+
+app.get('/img/:file', (req, res) => {
+  
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
