458eb4e4ceaa1e04871932854480d6d90072b7d6
Blue <ray1997860102@gmail.com>
2021-01-13 10:11:35 +0800

    輸入指定檔名後，可以觀看圖片

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ app.post('/fileupload', (req, res) => {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       var oldPath = files.filetoupload.path;
-      var newPath = __dirname + '/img' + '/' + files.filetoupload.name;
+      var newPath = __dirname + '/img/' + files.filetoupload.name;
       var rawData = fs.readFileSync(oldPath)
       fs.writeFile(newPath, rawData, function (err) {
         if (err){
-          res.send('ERROR!');
+          res.status(400).send('沒檔案不能提交');
         }
         else {
           res.write('File uploaded and moved!');
@@ -28,7 +28,15 @@ app.post('/fileupload', (req, res) => {
 })
 
 app.get('/img/:file', (req, res) => {
+  var newPath = __dirname + '/img/' + req.params.file;
   
+  fs.readFile(newPath, (err, data) => {
+    if(err) {res.status(404).send('404 not found');}
+    else {
+      res.write(data);
+      res.end();
+    }
+  });
 })
 
 app.listen(port, () => {
