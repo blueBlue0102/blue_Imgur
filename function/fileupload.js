7a8745492e1de785f08123414690b5b39d7f6d34
Blue <ray1997860102@gmail.com>
2021-01-13 23:08:56 +0800

    儲存圖片的名稱不再是原來的名稱，而是獨一無二的字串

--- function/fileupload.js
+++ function/fileupload.js
@@ -1,18 +1,24 @@
 const formidable = require('formidable');
 const path = require('path');
 const fs = require('fs');
+const uniqueString = require('./generateUniqueString');
 
 function fileUpload(req, res) {
   var form = new formidable.IncomingForm();
   form.parse(req, function (err, fields, files) {
     var oldPath = files.filetoupload.path;
-    var newPath = path.join(__dirname, '..', 'img', files.filetoupload.name);
-    var rawData = fs.readFileSync(oldPath);
-    fs.writeFile(newPath, rawData, function (err) {
-      if (err){ res.status(400).send('沒檔案不能提交'); }
+    fs.readFile(oldPath, (err, data) => {
+      if (err) {res.status(400).send('沒檔案不能提交');}
       else {
-        res.write('File uploaded and moved!');
-        res.end();
+        if (files.filetoupload.name === '') {res.status(400).send('沒檔案不能提交');return};
+        var newPath = path.join(__dirname, '..', 'img', uniqueString() + '.' + files.filetoupload.name.split('.').pop());
+        fs.writeFile(newPath, data, function (err) {
+          if (err){ res.status(400).send('沒檔案不能提交'); }
+          else {
+            res.write('File uploaded and moved!');
+            res.end();
+          }
+        });
       }
     });
   });
