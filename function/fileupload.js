469d59a9af559d06a91cd8efb0d0758dec7ad558
Blue <ray1997860102@gmail.com>
2021-01-14 12:34:18 +0800

    "formidable" 套件上傳檔案，預設最大的檔案大小為 200 MB 新增了"檔案大小超過上限"的處理方式，之前傳送過大的檔案會造成伺服器崩潰

--- function/fileupload.js
+++ function/fileupload.js
@@ -8,15 +8,16 @@ require('express').Router().use(require('./displayImg'));
 function fileUpload(req, res) {
   var form = new formidable.IncomingForm();
   form.parse(req, function (err, fields, files) {
+    if (err) {res.status(400).send(err.message);};
     var oldPath = files.filetoupload.path;
     fs.readFile(oldPath, (err, data) => {
-      if (err) {res.status(400).send('沒檔案不能提交');}
+      if (err) {res.status(400).send(err.message);}
       else {
         if (files.filetoupload.name === '') {res.status(400).send('沒檔案不能提交');return};
         const newFileName = uniqueString() + '.' + files.filetoupload.name.split('.').pop();
         var newPath = path.join(__dirname, '..', 'img', newFileName);
         fs.writeFile(newPath, data, function (err) {
-          if (err){ res.status(400).send('沒檔案不能提交'); }
+          if (err){ res.status(500).send(err.message); }
           else {
             res.redirect(newFileName);
           }
