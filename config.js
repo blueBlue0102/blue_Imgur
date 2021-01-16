1315572fdb3b850a2b076b0ca885ba58c9f9e1fd
Blue <ray1997860102@gmail.com>
2021-01-16 19:44:00 +0800

    伺服器依據檔名來判斷是否為圖片
    目前只容許：
    jpg / jpeg
    png
    gif
    
    檔案上限為 5 MB

--- config.js
+++ config.js
@@ -3,5 +3,11 @@ const config = {};
 config.port = 8080;
 config.imgPath = '/img';    //儲存圖片的地方，與根目錄的相對路徑
 config.maxSizeOfData = 5 * 1024 * 1024;  // 5 MB
+config.mime = {
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    jpeg: 'image/jpeg',
+    png: 'image/png',
+};
 
 module.exports = config;
--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 const formidable = require('formidable');
 const path = require('path');
 const fs = require('fs');
+const Joi = require('joi');
 const config = require('./config');
 const uniqueString = require('./function/generateUniqueString');
 
@@ -17,8 +18,9 @@ exports.fileUpload = (req, res) => {
       fs.readFile(oldPath, (err, data) => {
         if (err) {res.status(400).send(err.message); return;}
         else {
-          if (files.filetoupload.name === '') {res.status(400).send('沒檔案不能提交'); return};
-          const newFileName = uniqueString() + '.' + files.filetoupload.name.split('.').pop();
+          const fileType = config.mime[path.extname(files.filetoupload.name).slice(1).toLowerCase()] || 'WRONG';
+          if (fileType === 'WRONG') {res.status(400).send('錯誤的檔案格式！'); return;};
+          const newFileName = uniqueString() + path.extname(files.filetoupload.name);
           const newPath = path.join(__dirname, config.imgPath, newFileName);
           fs.writeFile(newPath, data, function (err) {
             if (err) {res.status(500).send(err.message); return;} 
