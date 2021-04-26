dfec88e571585565c183d821632bd33d82c6eb5d
Blue <ray1997860102@gmail.com>
2021-04-26 17:14:23 +0800

    Let URL of image more randomly to prevent URL collision

--- function/generateUniqueString.js
+++ function/generateUniqueString.js
@@ -1,13 +1,25 @@
 function generateUniqueString() {
-    var ts = String(new Date().getTime()),
-        i = 1,
-        out = '';
-
-    for (i = 1; i < ts.length; i += 3) {
-        out += Number(ts.substr(i, 3)).toString(36);
+    let result = [];
+  
+    // generate string by random
+    const length = 3;
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (let i = 0; i < length; i++) {
+      result.push(
+        characters.charAt(Math.floor(Math.random() * characters.length))
+      );
     }
-
-    return (out);
-}
+  
+    // generate string by time
+    let ts = String(new Date().getTime());
+  
+    for (let i = 1; i < ts.length; i += 3) {
+      result.push(Number(ts.substr(i, 3)).toString(36));
+    }
+  
+    return result.join("");
+  }
 
 module.exports = generateUniqueString;
