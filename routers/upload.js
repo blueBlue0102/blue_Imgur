b01b2bdc481862761fafa7b23a301fe797dceaeb
Blue <ray1997860102@gmail.com>
2021-04-29 15:19:31 +0800

    version 2.0.0
    - Using 'dropzone@5.7.0' to change the frontend
      add feature: upload file by 'paste' from clipboard
      NEED TO MODIFY 'dropzone.js' TO ENABLE THIS FEATURE
    - refactoring the entire project

--- routers/upload.js
+++ routers/upload.js
@@ -1,49 +1,60 @@
 const express = require("express");
 const formidable = require("formidable");
 const path = require("path");
-const fs = require("fs");
-const uniqueString = require("../function/generateUniqueString");
 
 const router = express.Router();
 
-const mime = {
-  gif: "image/gif",
-  jpg: "image/jpeg",
-  jpeg: "image/jpeg",
-  png: "image/png",
-  jfif: "image/jfif",
-  mp4: "video/mp4",
-  mov: "video/mov",
-  pdf: "pdf",
+const FORMIDABLE_CONFIG = {
+  uploadDir: path.join(
+    path.dirname(require.main.filename),
+    process.env.IMAGE_STORAGE_PATH
+  ),
+
+  keepExtensions: true,
+
+  allowEmptyFiles: false,
+
+  maxFileSize: Number(process.env.DATA_MAX_SIZE) * 1024 * 1024,
+
+  filename: function (name, ext, part, form) {
+    let result = [];
+
+    // generate string by Math.random()
+    const length = 3;
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++) {
+      result.push(
+        characters.charAt(Math.floor(Math.random() * characters.length))
+      );
+    }
+
+    // generate string by Date().getTime()
+    let ts = String(new Date().getTime());
+
+    for (let i = 1; i < ts.length; i += 3) {
+      result.push(Number(ts.substr(i, 3)).toString(36));
+    }
+
+    return result.join("") + ext;
+  },
+
+  // filter: function ({ name, originalFilename, mimetype }) {
+  //   return mimetype && mimetype.includes("image");
+  // },
 };
 
-router.post("/", (req, res) => {
-  const form = new formidable.IncomingForm();
-  form.maxFileSize = process.env.DATA_MAX_SIZE;
+router.post("/", async (req, res, next) => {
+  const form = formidable(FORMIDABLE_CONFIG);
   form.parse(req, function (err, fields, files) {
-    if (err) return res.status(400).end(err.message);
-    const oldPath = files.filetoupload.path;
-    fs.readFile(oldPath, (err, data) => {
-      if (err) return res.status(400).end(err.message);
-      else {
-        const fileType =
-          mime[path.extname(files.filetoupload.name).slice(1).toLowerCase()] ||
-          "WRONG";
-        if (fileType === "WRONG")
-          return res.status(400).end("錯誤的檔案格式！");
-        const newFileName =
-          uniqueString() + path.extname(files.filetoupload.name).toLowerCase();
-        const newPath = path.join(
-          path.dirname(require.main.filename),
-          process.env.IMAGE_STORAGE_PATH,
-          newFileName
-        );
-        fs.writeFile(newPath, data, function (err) {
-          if (err) return res.status(500).end(err.message);
-          else res.end(newFileName);
-        });
-      }
-    });
+    if (err) {
+      console.log(err.message);
+      return res.status(400).end(err.message);
+    }
+    console.log("parsing");
+    console.log("files:", files.file.mimetype);
+    return res.status(200).render("upload");
   });
 });
 
