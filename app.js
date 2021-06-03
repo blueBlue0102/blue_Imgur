6fcb4a7041740444f2e29a2e09f39c7c6b5016d3
Blue <ray1997860102@gmail.com>
2021-06-03 18:59:49 +0800

    refactoring app.js to make it seem to be clearer

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 require("dotenv").config();
-const express = require("express");
 const path = require("path");
-const session = require("express-session");
-const passport = require("passport");
-const redis = require("redis");
-const RedisStore = require("connect-redis")(session);
+
+// logger
 const winston = require("winston");
 const { format } = winston;
-
 winston.configure({
   level: "info",
   format: format.combine(format.timestamp(), format.json()),
@@ -18,12 +14,20 @@ winston.configure({
   ],
 });
 
+// express
+const express = require("express");
 const app = express();
-const redisClient = redis.createClient(process.env.REDIS_URL);
-
 app.use(express.static(path.join(__dirname, process.env.IMAGE_STORAGE_PATH)));
 app.use(express.static("dist"));
 app.use(require("body-parser").urlencoded({ extended: true }));
+
+// express session
+const session = require("express-session");
+
+// Redis
+const redis = require("redis");
+const RedisStore = require("connect-redis")(session);
+const redisClient = redis.createClient(process.env.REDIS_URL);
 app.use(
   session({
     store: new RedisStore({ client: redisClient }),
@@ -34,21 +38,20 @@ app.use(
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 365, httpOnly: true },
   })
 );
+
+// Passport
+const passport = require("passport");
 app.use(passport.initialize());
 app.use(passport.session());
 
+// ejs
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
 // routes
 require("./routers/index")(app);
 
+// server
 let server = app.listen(process.env.PORT, () => {
   console.log(`App listening at ${process.env.SERVER_HOST_NAME}`);
-});
-
-process.on('error', (err, socket) => {
-  console.log("app.js 'error' emit!!");
-  console.log(err);
-  // socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
